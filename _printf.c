dccba5518b98bd7b5f47412944a9d5b9647c12fd
masmiyen <masmiyen@gmail.com>
2023-04-24 09:50:22 -0700

    printf c

---
+++ _printf.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ *
+ * @format: string with all characters
+ *
+ * Return: prin chars
+ */
+int _printf(const char * format, ...)
+{
+	int print_chars;
+	convert_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"u", unsigned_int},
+		{"%", print_percentage},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_hex},
+		{"R", rot13},
+		{NULL, NULL}
+	};
+	va_list arg_list;
+	
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg_list, format);
+
+	print_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+
+	return (print_chars);
+}
