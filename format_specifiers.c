2b116bcf195b63b51b44cc347e35f54b7a0be030
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-04-25 02:08:07 +0100

    printf

--- format_specifiers.c
+++ format_specifiers.c
@@ -81,3 +81,24 @@ int unsigned_int(va_list list)
     if (num == 0)
         return (print_unsigned_number(num))
 }
+/**
+ * rev_string - Reverse a string
+ * @s: The string to reverse
+ */
+void rev_string(char *s)
+{
+	int i, length;
+	char tmp;
+
+	length = 0;
+	for (i = 0; s[i] != '\0'; i++)
+		length++;
+
+	for (i = 0; i < length; i++)
+	{
+		tmp = s[i];
+		s[i] = s[length - 1];
+		s[length - 1] = tmp;
+		length--;
+	}
+}
