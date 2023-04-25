f7b5d6c0f43a0369acf2d3f76455e0f94863f3ec
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-04-25 02:00:28 +0100

    printf

--- base_changes.c
+++ base_changes.c
@@ -92,3 +92,47 @@ int print_octal(va_list list)
 	free(revStr);
 	return (len);
 }
+/**
+ * print_hex - Prints  decimal number on base16 lowercase
+ * @list: List of the arguments
+ * Return: Number of characters
+ */
+int print_hex(va_list list)
+{
+	int len;
+	int rem_num;
+    unsigned int num;
+	char *hex_rep;
+	char *rev_hex;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 16);
+	hex_rep = malloc(sizeof(char) * len + 1);
+	if (hex_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		rem_num = num % 16;
+		if (rem_num > 9)
+		{
+			rem_num = hex_check(rem_num, 'x');
+			hex_rep[len] = rem_num;
+		}
+		else
+			hex_rep[len] = rem_num + 48;
+		num = num / 16;
+	}
+	hex_rep[len] = '\0';
+	rev_hex = rev_string(hex_rep);
+	if (rev_hex == NULL)
+		return (-1);
+	write_base(rev_hex);
+	free(hex_rep);
+	free(rev_hex);
+	return (len);
+}
