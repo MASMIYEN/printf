755b85b6bf61f00c22d27f6e5b3531b694c2f93b
Skurf <atifyr0@gmail.com>
2023-04-24 17:21:13 -0700

    tasksdone

--- main.h
+++ main.h
@@ -1,28 +1,27 @@
 #ifndef PRINT_F
 #define PRINT_F
 
-#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
 /**
- * struct convert - a function to define a struct for functions and symbols
- *
- * @symb: operator
- * @func: function
- */
-
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
 struct convert
 {
-	char *symb;
-	int (*func)(va_list);
+	char *sym;
+	int (*f)(va_list);
 };
 typedef struct convert conver_t;
 
-/* main functions*/
+/*Main functions*/
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
-int parser(const char *format, conver_t func_list[], va_list arg_list);
-int write_char(char);
+int _write_char(char);
 int print_char(va_list);
 int print_string(va_list);
 int print_percent(va_list);
@@ -31,16 +30,17 @@ int print_number(va_list);
 int print_binary(va_list);
 int print_reversed(va_list arg);
 int rot13(va_list);
-int unsigned_int(va_list);
+int unsigned_integer(va_list);
 int print_octal(va_list list);
 int print_hex(va_list list);
 int print_heX(va_list list);
 
-/*Additional functions*/
+/*Helper functions*/
 unsigned int base_len(unsigned int, int);
 char *rev_string(char *);
 void write_base(char *str);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int print_unsgined_number(unsigned int);
+
 
 #endif
