6b69d1adcdda3f840615e569f5c6d820eed59a07
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2023-04-26 16:58:07 +0100

    buffer update

--- main.h
+++ main.h
@@ -97,7 +97,7 @@ int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
 	int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision,
+int write_num(int ind, char bffr[], int flags, int width, int precision,
 	int length, char padding, char extra_ch);
 int write_pointer(char buffer[], int ind, int length,
 	int width, int flags, char padding, char extra_ch, int padding_start);
